df07a5562b7bacd667f73e78d509087a2f93d294
Subash Muthuramalingam <143281354+SubashTheDAX@users.noreply.github.com>
2025-12-25 12:38:07 +0530

    Fix repo_id format in upload_folder call

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -4,7 +4,8 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
-    repo_id="TheHumanAgent/tour_pkg_predictor_app",          # the target repo
+    repo_id="TheHumanAgent/tour-pkg-predictor-app",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
+
